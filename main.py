abfaeb92bb1e311234c45b4ed932e5c3f432c20a
Sharif Adal <sharifadal2008@gmail.com>
2026-07-25 22:57:48 +0600

    feat(docs-loader): Add web module and switch to PDF loading

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 from dotenv import load_dotenv
 from langchain_mistralai import ChatMistralAI
-from langchain_community.document_loaders import TextLoader
+from langchain_community.document_loaders import PyPDFLoader
 from langchain_core.prompts import ChatPromptTemplate
 
 load_dotenv()
 
 
-data = TextLoader("docs_loader/notes.txt")
+data = PyPDFLoader("docs_loader/GRU.pdf")
 docs = data.load()
 
 template = ChatPromptTemplate.from_messages(
@@ -14,7 +14,7 @@ template = ChatPromptTemplate.from_messages(
 )
 
 model = ChatMistralAI(model="mistral-small-2506")
-prompt = template.format_messages(data=docs[0].page_content)
+prompt = template.format_messages(data=docs[0])
 
 result = model.invoke(prompt)
 print(result.content)
