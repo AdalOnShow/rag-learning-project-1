74cdb6ba5209aa64cfad537fb97f9439e53d63e1
Sharif Adal <sharifadal2008@gmail.com>
2026-07-25 22:45:39 +0600

    feat(main): Add document loading and AI summarization pipeline

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 from dotenv import load_dotenv
 from langchain_mistralai import ChatMistralAI
+from langchain_community.document_loaders import TextLoader
+from langchain_core.prompts import ChatPromptTemplate
 
 load_dotenv()
 
-model = ChatMistralAI(model="mistral-small-2506")
 
-res = model.invoke("Hello! who are you?")
-print(res.content)
+data = TextLoader("docs_loader/notes.txt")
+docs = data.load()
+
+template = ChatPromptTemplate.from_messages(
+    [("system", "you are a ai that summarizes the text"), ("human", "{data}")]
+)
+
+model = ChatMistralAI(model="mistral-small-2506")
+prompt = template.format_messages(data=docs[0].page_content)
+
+result = model.invoke(prompt)
+print(result.content)
