c1bd60319aca33811a34dd7fbde0634c4f4a5e91
Sharif Adal <sharifadal2008@gmail.com>
2026-07-26 21:37:33 +0600

    feat(docs-loader): Switch to Deep Learning PDF and add document chunking

--- main.py
+++ main.py
@@ -2,19 +2,23 @@ from dotenv import load_dotenv
 from langchain_mistralai import ChatMistralAI
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_core.prompts import ChatPromptTemplate
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 load_dotenv()
 
 
-data = PyPDFLoader("docs_loader/GRU.pdf")
+data = PyPDFLoader("docs_loader/deeplearning.pdf")
 docs = data.load()
+
+splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=20)
+chunks = splitter.split_documents(docs)
 
 template = ChatPromptTemplate.from_messages(
     [("system", "you are a ai that summarizes the text"), ("human", "{data}")]
 )
 
 model = ChatMistralAI(model="mistral-small-2506")
-prompt = template.format_messages(data=docs[0])
+prompt = template.format_messages(data=docs)
 
 result = model.invoke(prompt)
 print(result.content)
