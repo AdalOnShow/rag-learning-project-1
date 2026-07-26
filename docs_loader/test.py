986d614ce0c608bed774f27be6bbd92868c00dc8
Sharif Adal <sharifadal2008@gmail.com>
2026-07-26 21:02:42 +0600

    feat(docs-loader): Add text chunking and improve test output formatting

--- docs_loader/test.py
+++ docs_loader/test.py
@@ -1,6 +1,16 @@
 from langchain_community.document_loaders import TextLoader
+from langchain_text_splitters import CharacterTextSplitter
 
+splitter = CharacterTextSplitter(separator="", chunk_size=80, chunk_overlap=1)
 data = TextLoader("docs_loader/notes.txt")
 docs = data.load()
+chunks = splitter.split_documents(docs)
 
-print(docs)
+for chunk in chunks:
+    print(chunk.page_content)
+    print()
+    print()
+    print()
+
+
+# print(docs)
