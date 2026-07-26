3f6a0515d77aec5114cfe07e0a3f20514cc491ca
Sharif Adal <sharifadal2008@gmail.com>
2026-07-26 21:27:41 +0600

    feat(docs-loader): Add document chunking with RecursiveCharacterTextSplitter

--- docs_loader/pdf.py
+++ docs_loader/pdf.py
@@ -1,6 +1,11 @@
 from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 data = PyPDFLoader("docs_loader/GRU.pdf")
 docs = data.load()
+splitter = RecursiveCharacterTextSplitter(chunk_size=200, chunk_overlap=2)
+chunks = splitter.split_documents(docs)
 
-print(docs[14])
+print(chunks[20].page_content)
+
+# print(docs[14])
